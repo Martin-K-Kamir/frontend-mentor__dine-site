5b02c04221d01ee2fc7582b453333a85b381b126
Martin Kamír <martin.k.kamir@gmail.com>
2022-12-16 16:59:51 +0100

    w

--- src/react/components/FooterSite.jsx
+++ src/react/components/FooterSite.jsx
@@ -5,44 +5,42 @@ import Icon from "./Icon.jsx";
 export default function FooterSite() {
     return (
         <footer className="[ footer-site ] [ bg-neutral-2 fg-neutral-1 ]">
-            <div className="[ wrapper stack ] [ space-7 text-center//below-md ] ">
-                <div className="[ flow ] [ space-7 items-justify-center//below-md direction-row//above-md ]">
+            <div className="[ wrapper flow ] [ space-6 text-center//below-md ]" data-flow-childs='divs'>
+                <div className="items-justify-center//below-md direction-row//above-md">
                     <Link to="/" aria-label="homepage"><Logo/></Link>
-                    <div className="[ flow ] [ space-7 fs-1 text-uppercase letter-spacing-5 margin-center direction-row//above-lg ]">
-                        <div className="stack">
+                    <div className="fs-1 text-uppercase letter-spacing-5 direction-row//above-lg">
+                        <div data-inherit-space="false">
                             <p aria-label="restaurant address">Marthwaite, Sedbergh</p>
                             <p>Cumbria</p>
                             <a aria-label="phone number" href="tel:+00 44 123 4567">+00 44 123 4567</a>
                         </div>
-                        <div className="stack">
+                        <div data-inherit-space="false">
                             <p>open times</p>
                             <p>MON - FRI: 09:00 AM - 10:00 PM</p>
                             <p>SAT - SUN: 09:00 AM - 11:30 PM</p>
                         </div>
                     </div>
                 </div>
-                <div>
-                    <div className="[ flow ] [ space-2 items-justify-center//below-md items-align-center//below-md direction-row//above-md justify-between//above-md ]">
-                        <p className="fs-1">Challenge by <a href="https://www.frontendmentor.io/home">Frontend&nbsp;Mentor</a>.
-                            Coded&nbsp;by&nbsp;Martin&nbsp;Kamír.</p>
-                        <ul className="cluster" role="list">
-                            <li>
-                                <a rel="noreferrer" target="_blank" href="https://martinkamir.com/" aria-label="author's website">
-                                    <Icon name="mkk"/>
-                                </a>
-                            </li>
-                            <li>
-                                <a rel="noreferrer" target="_blank" href="https://github.com/Martin-K-Kamir" aria-label="github">
-                                    <Icon name="github"/>
-                                </a>
-                            </li>
-                            <li>
-                                <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/martin-kam%C3%ADr-2967aa222/" aria-label="linkedin">
-                                    <Icon name="linkedin"/>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
+                <div className="items-justify-center//below-md items-align-center//below-md direction-row//above-md justify-between//above-md" data-inherit-space="false">
+                    <p className="fs-1">Challenge by <a href="https://www.frontendmentor.io/home">Frontend&nbsp;Mentor</a>.
+                        Coded&nbsp;by&nbsp;Martin&nbsp;Kamír.</p>
+                    <ul className="cluster" role="list">
+                        <li>
+                            <a rel="noreferrer" target="_blank" href="https://martinkamir.com/" aria-label="author's website">
+                                <Icon name="mkk"/>
+                            </a>
+                        </li>
+                        <li>
+                            <a rel="noreferrer" target="_blank" href="https://github.com/Martin-K-Kamir" aria-label="github">
+                                <Icon name="github"/>
+                            </a>
+                        </li>
+                        <li>
+                            <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/martin-kam%C3%ADr-2967aa222/" aria-label="linkedin">
+                                <Icon name="linkedin"/>
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </footer>
--- src/react/components/Hero.jsx
+++ src/react/components/Hero.jsx
@@ -11,12 +11,12 @@ export default function Hero(props) {
                    fetchPriority={"high"}
                    resolutionW={[[1440, 2880], [768, 1536], [375, 750]]}
             />
-            <div className="[ wrapper flow ] [ items-justify-center//below-lg direction-row//above-lg ]">
-                <div className=" [ hero__container ] [ flow ] [ space-6 items-justify-center//below-lg align-center//below-lg ]">
+            <div className="[ wrapper flow ] [ space-6 items-justify-center//below-lg direction-row//above-lg ]">
+                <div className=" [ hero__container ] [ flow ] [ items-justify-center//below-lg align-center//below-lg ]">
                     <Link to="/" aria-label="homepage"><Logo/></Link>
-                    <div className="[ hero__content ] [ stack ] [ space-6 text-center//below-lg items-align-center//below-lg items-align-start//above-lg ]">
+                    <div className="[ hero__content ] [ stack ] [ text-center//below-lg items-align-center//below-lg items-align-start//above-lg ]">
                         <h1 className="title-4">{props.title}</h1>
-                        <p className="measure-4 space-1">{props.desc}</p>
+                        <p className="measure-4" data-inherit-space='false'>{props.desc}</p>
                         {props.button && <Button to={props.button.to} content={props.button.content} type="ghost"/>}
                     </div>
                 </div>
