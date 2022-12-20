b4a5ee05aaba6e6edb7e70b355366d2162839ef6
Martin Kamír <martin.k.kamir@gmail.com>
2022-12-20 13:45:47 +0100

    w

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,9 @@
-// const path = require('path');
+const path = require('path');
 
-// module.exports = () => {
-    // return {
-        // plugins: [
+module.exports = () => {
+    return {
+        plugins: [
+            require('postcss-preset-env')
             // require('postcss-variable-compress')(
                 // ['--_top', '--_right', '--_bottom', '--_left', '--_x', '--_y', '--_layer', '--_radius',
                     // '--_radius-1', '--_radius-2', '--_radius-3', '--_radius-4', '--_length']
@@ -37,6 +38,6 @@
             //         }
             //     }
             // }),
-        // ],
-    // };
-// };
+        ],
+    };
+};
