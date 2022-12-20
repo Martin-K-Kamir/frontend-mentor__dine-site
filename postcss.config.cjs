6e7ffd1003a94174c2ba032ad4f6b571f41f681c
Martin Kamír <martin.k.kamir@gmail.com>
2022-12-20 13:12:21 +0100

    w

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,14 +1,14 @@
-const path = require('path');
+// const path = require('path');
 
-module.exports = () => {
-    return {
-        plugins: [
-            require('postcss-variable-compress')(
-                ['--_top', '--_right', '--_bottom', '--_left', '--_x', '--_y', '--_layer', '--_radius',
-                    '--_radius-1', '--_radius-2', '--_radius-3', '--_radius-4', '--_length']
-            ),
-            require("postcss-combine-media-query"),
-            require("postcss-combine-duplicated-selectors"),
+// module.exports = () => {
+    // return {
+        // plugins: [
+            // require('postcss-variable-compress')(
+                // ['--_top', '--_right', '--_bottom', '--_left', '--_x', '--_y', '--_layer', '--_radius',
+                    // '--_radius-1', '--_radius-2', '--_radius-3', '--_radius-4', '--_length']
+            // ),
+            // require("postcss-combine-media-query"),
+            // require("postcss-combine-duplicated-selectors"),
             // require('postcss-extract-media-query') ({
             //     output: {
             //         path: path.join(__dirname, 'public/assets/css')
@@ -37,6 +37,6 @@ module.exports = () => {
             //         }
             //     }
             // }),
-        ],
-    };
-};
+        // ],
+    // };
+// };
