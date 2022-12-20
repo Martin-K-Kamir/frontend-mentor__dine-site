0faf34f5e68b3f70eeb73f0bc3acbf5a75b58ade
Martin Kamír <martin.k.kamir@gmail.com>
2022-12-20 13:53:51 +0100

    Update postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,13 +3,13 @@ const path = require('path');
 module.exports = () => {
     return {
         plugins: [
-            require('postcss-preset-env')
-            // require('postcss-variable-compress')(
-                // ['--_top', '--_right', '--_bottom', '--_left', '--_x', '--_y', '--_layer', '--_radius',
-                    // '--_radius-1', '--_radius-2', '--_radius-3', '--_radius-4', '--_length']
-            // ),
-            // require("postcss-combine-media-query"),
-            // require("postcss-combine-duplicated-selectors"),
+            require('postcss-preset-env'),
+            require('postcss-variable-compress')(
+                ['--_top', '--_right', '--_bottom', '--_left', '--_x', '--_y', '--_layer', '--_radius',
+                    '--_radius-1', '--_radius-2', '--_radius-3', '--_radius-4', '--_length']
+            ),
+            require("postcss-combine-media-query"),
+            require("postcss-combine-duplicated-selectors"),
             // require('postcss-extract-media-query') ({
             //     output: {
             //         path: path.join(__dirname, 'public/assets/css')
